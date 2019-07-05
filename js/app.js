7ba6d37df25a2101dd08da04988b7e6e951f9573
twardcox <t.ward.cox@gmail.com>
2019-07-05 14:37:53 -0700

    filters were mass populating with each page change. fixed bug

--- js/app.js
+++ js/app.js
@@ -1,37 +1,64 @@
 'use strict';
 
+// shows and hides elements based on selection
 $('select').on('change', function() {
+  // hides default value
   if (this.value === 'default') {
     $(`main > section`).show();
     $(`main > section:first-child`).hide();
   } else {
-    $(`main > section`).show();
+    // shows selected images
+    $(`main > select`).show();
 
     let $item = this.value;
+
     // eslint-disable-next-line no-unused-vars
+    // hides values not selected
     let $sectionEls = $(`main > section:not([value=${$item}])`).hide();
   }
 });
 
 let filePath = './data/page-1.json';
 
+// page change
 $('a').on('click', function(event) {
   event.preventDefault();
+
+  // clears out options list
+  $('header > select#select-template').empty();
+  $('header > select#select-options').empty();
+
+  const setTypeLabel = () => {
+    const $typeLabel = $('<option></option');
+    $typeLabel.attr('value', 'default').text('Filter by Keyword');
+    $('header > select#select-template').append($typeLabel);
+  };
+
+  const setOptionsLabel = () => {
+    const $optionsLabel = $('<option></option');
+    $optionsLabel.attr('value', 'default2').text('Filter by Horns');
+    $('header > select#select-options').append($optionsLabel);
+  };
+
   console.log('a on click');
   if (filePath === './data/page-1.json') {
     $('a').text('Page 2');
     $('main').empty();
     allAnimals = [];
     filePath = './data/page-2.json';
+
+    setTypeLabel();
+    setOptionsLabel();
+
     Animal.getAllAnimalsFromFile();
-    console.log('filePath page 1: ', filePath);
   } else {
     filePath = './data/page-1.json';
     $('a').text('Home');
     $('main').empty();
     allAnimals = [];
+    setTypeLabel();
+    setOptionsLabel();
     Animal.getAllAnimalsFromFile();
-    console.log('filePath page 2: ', filePath);
   }
 });
 
@@ -55,22 +82,29 @@ Animal.prototype.renderwithHandlebars = function() {
   $('main').prepend(html);
 };
 
+// creates option list
 Animal.prototype.filterWithJQuery = function() {
-  const $selectEl = $('<option></option>');
-  $selectEl.attr('value', this.keyword).text(this.keyword);
+  // option list for creature keywor
+  const $selectType = $('<option></option>');
+  $selectType.attr('value', this.keyword).text(this.keyword);
 
-  $('header>select').append($selectEl);
+  $('header > select#select-template').append($selectType);
+
+  // option list for number of horns
+  const $selectHorns = $('<option></option>');
+  $selectHorns.attr('value', this.horns).text(this.horns);
+
+  $('header > select#select-options').append($selectHorns);
 };
 
 Animal.getAllAnimalsFromFile = () => {
   // logic that id's the page we are on
-  // const filePath = $('body').attr('id') === 'index' ? './data/page-1.json' : '../data/page-2.json';
+
   console.log('get animals filePath: ', filePath);
 
   const fileType = 'json';
   $.get(filePath, fileType).then(myAnimalJSON => {
     myAnimalJSON.forEach(animal => {
-      // new Animal(animal.title, animal.url, animal.description, animal.keyword, animal.horns);
       allAnimals.push(new Animal(animal.title, animal.image_url, animal.description, animal.keyword, animal.horns));
     });
 
